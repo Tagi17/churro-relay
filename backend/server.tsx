9a20a98a5d0a1c4fa886d53821780e6be11f6868
Nazia <naziaozair@gmail.com>
2024-02-16 16:21:52 -0500

    Initial commit with frontend and backend directories

---
+++ backend/server.tsx
@@ -0,0 +1,9 @@
+const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const app = express()
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log('Server running on port ${port}'));
+app.use('/api/auth', authRoutes);
